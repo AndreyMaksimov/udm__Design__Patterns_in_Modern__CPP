1d413ecff6ae2ac2c08ee632f772d9ee8a263e24
Andrey Maksimov <andrey.maksimov@gmail.com>
2025-02-23 00:53:51 +0300

    Added google test to build

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -2,6 +2,7 @@
 function(build_sample sample_name)
     set(sample_target ${sample_name}.exe)
     add_executable(${sample_target} ${sample_name}.cpp )
+    target_link_libraries(${sample_target} PUBLIC ${GTEST_LIB})
 endfunction()
 
 # Obsolete as soon we installed boost to a normal place /usr/include/boost/...
